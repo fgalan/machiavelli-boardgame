ea20a2bb4c7420f862261b697ac7235be6c7e257
fgalan@gmail.com <fgalan@gmail.com@6fa414a6-685b-4ef6-5c20-73700029ea60>
2011-02-17 23:50:40 +0000

    - Stub for Engine class turn processing methods - Processing of "starting spring" completed

--- machiavelli-boardgame/src/Test/Tester.java
+++ machiavelli-boardgame/src/Test/Tester.java
@@ -28,49 +28,39 @@ public class Tester {
 			GameStatus gs;
 			FileWriter f;
 			
-			m = new Map(new File("D://eclipse-my-projects//machiavelli-gameboard//xml//initial-scenario-1.xml"));
-			gs = new GameStatus(new File("D://eclipse-my-projects//machiavelli-gameboard//xml//game_status.xml"));
+			m = new Map(new File("D://eclipse-my-projects//machiavelli-gameboard//xml//The-Balance-Of-Power-SCENARIO.xml"));
+			gs = new GameStatus(new File("D://eclipse-my-projects//machiavelli-gameboard//xml//The-Balance-Of-Power-GAMESTATUS.xml"));
 			
 			/* Just to get provinces in alphabetical order, so we can compare maps XML easily */
 			f = new FileWriter("D://eclipse-my-projects//machiavelli-gameboard//xml//test-0.xml");
 			f.write(m.toXml());
 			f.close();
-			
-			System.out.println("---- Doing famine -----");
-			System.out.print(Engine.doFamine(m));
-			f = new FileWriter("D://eclipse-my-projects//machiavelli-gameboard//xml//test-1.xml");
-			f.write(m.toXml());
-			System.out.println(m.toString());
-			f.close();
-			
-			System.out.println("---- Clearing famine -----");			
-			System.out.print(Engine.clearFamine(m));
-			f = new FileWriter("D://eclipse-my-projects//machiavelli-gameboard//xml//test-3.xml");
-			f.write(m.toXml());
-			System.out.println(m.toString());
-			f.close();
-			
-			System.out.println("---- Doing plague -----");			
-			System.out.print(Engine.doPlague(m));
-			f = new FileWriter("D://eclipse-my-projects//machiavelli-gameboard//xml//test-4.xml");
-			f.write(m.toXml());
-			System.out.println(m.toString());
-			f.close();
-						
+									
 			System.out.println(m.toString());
 			
-			f = new FileWriter("D://eclipse-my-projects//machiavelli-gameboard//xml//gs-0.xml");
+			/* Process the first turn */
+			System.out.println(Engine.processCommands(gs, m, null));
+			
+			f = new FileWriter("D://eclipse-my-projects//machiavelli-gameboard//xml/gs-new.xml");
 			f.write(gs.toXml());
 			f.close();
 			
+			/* Just to get provinces in alphabetical order, so we can compare maps XML easily */
+			f = new FileWriter("D://eclipse-my-projects//machiavelli-gameboard//xml//map-new.xml");
+			f.write(m.toXml());
+			f.close();			
+			
 			System.out.println(gs.getStatus("Austria"));
+			System.out.println(gs.getStatus("Florence"));			
 			System.out.println(gs.getStatus("France"));
+			System.out.println(gs.getStatus("Milan"));
+			System.out.println(gs.getStatus("Naples"));
 			System.out.println(gs.getStatus("Papacy"));
-			System.out.println(gs.getStatus("Florence"));
+			System.out.println(gs.getStatus("Turks"));
 			System.out.println(gs.getStatus("Venice"));
 			
-			Commands cms = new Commands(new File("D://eclipse-my-projects//machiavelli-gameboard//xml//Commands-example.xml"));
-			Adjustments adj = new Adjustments(new File("D://eclipse-my-projects//machiavelli-gameboard//xml//Adjustments-example.xml"));
+			//Commands cms = new Commands(new File("D://eclipse-my-projects//machiavelli-gameboard//xml//Commands-example.xml"));
+			//Adjustments adj = new Adjustments(new File("D://eclipse-my-projects//machiavelli-gameboard//xml//Adjustments-example.xml"));
 			
 		} catch (Exception e) {
 			e.printStackTrace();
