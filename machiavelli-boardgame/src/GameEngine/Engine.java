e483a7aae295c7780c6b9d10e1773aa580618420
fgalan@gmail.com <fgalan@gmail.com@6fa414a6-685b-4ef6-5c20-73700029ea60>
2011-02-19 21:48:07 +0000

    Engine processAdjustments method implemented

--- machiavelli-boardgame/src/GameEngine/Engine.java
+++ machiavelli-boardgame/src/GameEngine/Engine.java
@@ -28,14 +28,19 @@ import org.apache.log4j.Logger;
 import Exceptions.ProcessAdjustmentsException;
 import Exceptions.ProcessCommandsException;
 import Exceptions.UnknownCountryException;
+import GameElements.Army;
+import GameElements.Fleet;
+import GameElements.Garrison;
 import GameElements.City;
 import GameElements.Map;
 import GameElements.Province;
+import GameElements.Territory;
+import GameElements.Unit;
 
 public class Engine {
 	
 	private final static Logger log = Logger.getLogger("Engine.class");
-
+	
 	/**
 	 * Processes a set of Command orders (supposed each one from a different player) on the
 	 * map, in the context of a given game status.
@@ -119,20 +124,175 @@ public class Engine {
 	 */
 	public static Result processAdjustments(GameStatus gs, Map m, Vector<Adjustments> adj) throws ProcessAdjustmentsException {
 		
-		ComposedResult r = new ComposedResult();
+		GenericResult r = new GenericResult();
 		
-		/* This process only can be done in spring, so other campaing will throw an Exception */
+		/* This process only can be done in spring, so other campaign will throw an Exception */
 		if (gs.getCampaign() != GameStatus.SPRING) {
 			throw new ProcessAdjustmentsException("wrong campaing " + gs.getCampaign());
 		}
 		
-		for (Iterator<String> i = gs.getPlayers().iterator(); i.hasNext() ; i.next()) {
+		for (Iterator<Adjustments> i = adj.iterator(); i.hasNext() ; ) {
+			Adjustments adr = i.next();
+			r.addResult("player " + adr.getPlayer() + " adjustment:");
+			
+			/* Gather all the units belonging to player */
+			Vector<Unit> v = m.getUnitBelongingToPlayer(adr.getPlayer(),null);
 			
 			/* Process payments */
-			// TODO
+			for (Iterator<Payment> j = adr.getPayments().iterator(); j.hasNext(); ) {
+				Payment p = j.next();
+				String type = p.getType();
+				int id = p.getId();
+				
+				/* Does that Unit belong to the player? */
+				Unit u = null;
+				for (int k = 0; k < v.size(); k++) {
+					if (v.elementAt(k).getId() == id) {
+						if (type.equals("Army")) {
+							if (v.elementAt(k) instanceof Army) {
+								u = v.elementAt(k);
+							}
+						}
+						else if (type.equals("Fleet")) {
+							if (v.elementAt(k) instanceof Fleet) {
+								u = v.elementAt(k);
+							}
+						}
+						else { // Garrison
+							if (v.elementAt(k) instanceof Garrison) {
+								u = v.elementAt(k);
+							}
+						}
+						
+					}
+				}
+				if (u == null) {
+					r.addResult("- can not process payment order <>: player has no " + type.substring(0, 1) + id + "unit");
+					continue;
+				}
+				
+				/* Check money and pay */
+				if (u.cost() > gs.getMoney(adr.getPlayer())) {
+					r.addResult("- can not process payment order <>: not enough money");
+				}
+				else {
+					/* Pay */
+					gs.decMoney(adr.getPlayer(), u.cost());
+					r.addResult("- payment: "+u+" (-"+u.cost()+")");
+					
+					/* Remove Unit for the vector*/
+					v.remove(u);
+				}
+				
+			}
+			
+			/* All the unit remaining in the vector has to be removed. A Province vector is built
+			 * based on this, that is used in the next sub-phase (Purchase) */
+			Vector<Province> vp = new Vector<Province>();
+			for (Iterator<Unit> j = v.iterator() ; j.hasNext(); ) {
+				Unit u = j.next();
+				Territory t = u.getLocation();
+				r.addResult("- unit "+ u +" removed due to unpayment");
+				
+				/* Actually removing Unit from map */
+				if (u instanceof Army || u instanceof Fleet) {
+					u.getLocation().clearUnit();
+				}
+				else { // Garrison
+					((Province)u.getLocation()).getCity().clearUnit();
+				}
+				
+				if (t instanceof Province) {
+					vp.add((Province)t);
+				}
+			}
 			
 			/* Process purchases */
-			// TODO
+			for (Iterator<Purchase> j = adr.getPurchases().iterator(); j.hasNext(); ) {
+				Purchase p = j.next();
+				String type = p.getType();
+				Province pr = (Province) m.getTerritoryByName(p.getProvince());
+				
+				/* The player has not reach unit limit (A < 12, F < 8, G < 6) */
+				int freeId = m.GetFreeId(adr.getPlayer(), type);
+				if ( (type.equals("Army") && freeId == Army.MAX) || 
+				     (type.equals("Fleet") && freeId == Fleet.MAX) ||
+				     (type.equals("Garrison") && freeId == Garrison.MAX)
+				   ) {
+					r.addResult("- can not process purchase order <>: maximum units limit reached for " + type);
+					continue;
+				}
+				
+				/* Is the province valid?
+				 * a) Exists */
+				if (pr == null) {
+					r.addResult("- can not process purchase order <>: Province " +p.getProvince()+ "doesn't exists");
+					continue;
+				}
+				/* a) Belong to player */
+				if (pr.getController() == null || !pr.getController().equals(adr.getPlayer())) {
+					r.addResult("- can not process purchase order <>: Province " +p.getProvince()+ "doesn't belong to player");
+					continue;
+				}
+				/* b) Has a city (no matter if fortified or not, no matter city controller) */
+				if (pr.getCity() == null) {
+					r.addResult("- can not process purchase order <>: Province " +p.getProvince()+ "has no city");
+					continue;
+				}
+				/* c) For fleets, the city has port */
+				if (type.equals("Fleet") && !pr.getCity().isPort()) {
+					r.addResult("- can not process purchase order <>: Province " +p.getProvince()+ "has no city");
+					continue;
+				}				
+				/* d) No unpaid unit was removed in the province in the same adjustment phase */
+				if (vp.contains(pr)) {
+					r.addResult("- can not process purchase order <>: a unit was unpayed in the same province this turn, " +p.getProvince());
+					continue;
+				}
+				
+				/* For Army/Fleet, no other Army/Fleet is in the same province */
+				if ( (type.equals("Army") || type.equals("Fleet")) && pr.getUnit() != null) {
+					r.addResult("- can not process purchase order <>: existing unit in " +p.getProvince() + ", " + pr.getUnit());
+					continue;
+				}
+				
+				/* For Garrison, no other Garrison is in the same city */
+				if ( type.equals("Garrison") && pr.getCity().getUnit() != null) {
+					r.addResult("- can not process purchase order <>: existing unit in city in " +p.getProvince() + ", " + pr.getCity().getUnit());
+					continue;
+				}
+				
+				/* Pre-create the Unit (note that creation could fail beyond this point) */
+				Unit u;
+				if (type.equals("Army")) {
+					u = new Army(m.GetFreeId(adr.getPlayer(), type), adr.getPlayer(), null, p.getElite());
+				}
+				else if (type.equals("Fleet")) {
+					u = new Fleet(m.GetFreeId(adr.getPlayer(), type), adr.getPlayer(), null, p.getElite());
+				}
+				else { // garrison
+					u = new Garrison(m.GetFreeId(adr.getPlayer(), type), adr.getPlayer(), (City)null, p.getElite());
+				}
+				
+				/* Check money and buy */
+				if (u.cost() > gs.getMoney(adr.getPlayer())) {
+					r.addResult("- can not process buy order <>: not enough money");
+				}
+				else {
+					/* Pay */
+					gs.decMoney(adr.getPlayer(), u.cost());
+					r.addResult("- payment: "+u+" (-"+u.cost()+")");
+					
+					/* Attach unit to location in the map */
+					u.setLocation(pr);
+					if (u instanceof Army || u instanceof Fleet) {
+						pr.setUnit(u);
+					}
+					else { // garrison
+						pr.getCity().setUnit(u);
+					}
+				}
+			}
 		}
 		
 		return r;
--- machiavelli-boardgame/src/GameEngine/Payment.java
+++ machiavelli-boardgame/src/GameEngine/Payment.java
@@ -29,4 +29,12 @@ public class Payment {
 		type = t;
 		id = i;
 	}
+
+	public String getType() {
+		return type;
+	}
+
+	public int getId() {
+		return id;
+	}
 }
